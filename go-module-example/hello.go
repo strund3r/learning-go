1603b7ad8eb154603b94baa3df539d5477985ea0
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-09-16 16:50:05 -0300

    Go Module example

---
+++ go-module-example/hello.go
@@ -0,0 +1,18 @@
+package hello
+
+import "rsc.io/quote"
+
+// Hello func
+func Hello() string {
+	//return "Hello, world."
+	return quote.Hello()
+}
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	bytesnumber, errors := fmt.Println("Hello World!")
+// 	fmt.Println(bytesnumber, errors)
+// }
