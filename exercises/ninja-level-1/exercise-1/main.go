6d2331e8df1c5a4e2d71a777f35757e58d92090b
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-09-16 18:35:11 -0300

    Exercise 1

---
+++ exercises/ninja-level-1/exercise-1/main.go
@@ -0,0 +1,29 @@
+// 1. Using the short declaration operator,
+// ASSIGN these VALUES to VARIABLES with the IDENTIFIERS "x" and "y" and "z"
+//
+// a. 42
+// b. "James Bond"
+// c. true
+//
+// 2. Now print the values stored in those variables using
+//
+// a. a single print statement
+// b. multiple print statements
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// Single print statement
+	fmt.Println(x, y, z)
+
+	// Multiple print statements
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
